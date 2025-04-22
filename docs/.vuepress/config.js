5e94442d8ad7a98586a3da3538ef5bc3d5ed8d73
morepaperac <jasonaowu@163.com>
2025-04-22 22:11:39 +0800

    更新博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,14 @@
 module.exports = {
   // 其他配置...
+  head: [
+    // 其他头部标签...
+    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css' }],
+    ['script', { src: 'https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.js' }],
+    ['script', { src: 'https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/contrib/auto-render.min.js' }]
+  ],
   plugins: [
     'vuepress-plugin-katex'
-    // 或者 'vuepress-plugin-mathjax'
-  ]
+    // 或者使用 MathJax
+    // '@vuepress/plugin-mathjax'
+  ],
 }
